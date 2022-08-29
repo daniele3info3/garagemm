a05aa3d6d491c0d00cfa77cbc4ef6d7896f9ae4c
Daniele da Silva <silva.daniele.ti@gmail.com>
2022-08-29 15:11:01 -0300

    DRF

--- core/models.py
+++ core/models.py
@@ -7,12 +7,22 @@ class Categoria(models.Model):
     def __str__(self):
         return self.descricao
 
+    class Meta:
+        verbose_name_plural = "Categorias"
+
+    #marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="categoria")
+
+
 class Marca(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.nome
+
+    class Meta:
+        verbose_name_plural = "Marcas"
+
 
 class Cor(models.Model):
     nome = models.CharField(max_length=100)
@@ -23,6 +33,7 @@ class Cor(models.Model):
     class Meta:
         verbose_name_plural = "Cores"
 
+
 class Modelo(models.Model):
     nome = models.CharField(max_length=100)
 
@@ -31,4 +42,3 @@ class Modelo(models.Model):
 
     class Meta:
         verbose_name_plural = "Modelos"
-
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from core.models import Categoria, Marca
+from core.serializers import CategoriaSerializer, MarcaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer()
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer()
